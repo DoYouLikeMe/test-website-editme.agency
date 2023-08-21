e94b0b4d46b4fe11aa87cf0d4065d257dff2dac7
ALEXEY <avoronsib@gmail.com>
2023-08-21 17:28:42 +0300

    fix: mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,6 +3,10 @@ import {toggleScroll} from "./toggleScroll";
 const btnMobile = document.querySelector(".mobile-menu-button");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+window.onload = () => {
+  mobileMenu.classList.add("mobile-menu_animated");
+};
+
 const toggleMenu = function () {
   toggleScroll();
   btnMobile.classList.toggle("mobile-menu-button_active");
